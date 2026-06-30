629d2f2b0cef114b3b6bf7b4e10c899c3ab19cc7
smartadmins <ksudhy@smartadmins.in>
2026-06-30 11:26:42 +0530

    added src file

---
+++ src/main/java/com/smartadmins/App.java
@@ -0,0 +1,12 @@
+package com.cloudtechnet;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello smartadmins!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
